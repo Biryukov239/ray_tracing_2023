517c15b797870f67950cd41e0b16c3c76fdadaee
Biryukov239 <gregory239@mail.ru>
2023-12-05 02:48:06 +0300

    CMake, first PPM image

--- src/ray_tracing_simple/main.cpp
+++ src/ray_tracing_simple/main.cpp
@@ -6,6 +6,7 @@ int main() {
   int image_width = 256;
   int image_height = 256;
   // Render
+
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
   for (int j = 0; j < image_height; ++j) {
     std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
