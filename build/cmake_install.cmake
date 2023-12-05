2ca8da7db3aeca65704c738632fb6e2cb135c0b3
Biryukov239 <gregory239@mail.ru>
2023-12-05 20:26:01 +0300

    directory changes

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/gregory/ray_tracing_2023/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/gregory/ray_tracing_2023/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
